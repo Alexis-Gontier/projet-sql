64cbc98db16b167cde5a1d6eea9045cc77ca9e1e
gontier <alexis.gontier03@laposte.net>
2024-11-12 14:32:09 +0100

    ✨ base

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+import express from "express"
+import cors from "cors"
+import mysql from "mysql"
+
+const app = express()
+const PORT = 3000
+app.use(cors());
+app.use(express.json())
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "event"
+})
+
+db.connect((err) => {
+    if (err) {
+        console.log("Erreur de connexion à la BDD:\n", err)
+    } else {
+        console.log("Connecté !")
+    }
+})
+
+app.get("/", (req, res) => {
+    res.send("Hello world")
+})
+
+app.get("/data", (req, res) => {
+    const sql = "SELECT * FROM data"
+    db.query(sql, (err, result) => {
+        if (err) {
+            return res.status(500).json({
+                "status": 500,
+                "message": "Internal Server Error"
+              })
+        }
+        return res.json(result)
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Serveur up sur: http://localhost:${PORT}`)
+})
