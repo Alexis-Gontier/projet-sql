aacd9e17137b5330e50b2b2a0ce9ab0a0e469110
Alexis G <alexis.gontier03@gmail.com>
2024-11-16 00:14:45 +0100

    💄 test front

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,25 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
 export default function Home() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/v1/spectacles')
+      .then(res => setData(res.data))
+      .catch(err => console.log(err))
+  }, [])
+
   return (
-    <div className="font-bold text-4xl">Home</div>
-  )
+    <ul>
+      {data.map(spectacle => (
+        <li key={spectacle.id} className="p-5 m-5 border-2 max-w-sm">
+          <p className="text-xl">{spectacle.title}</p>
+          <p>{spectacle.synopsis}</p>
+          <p>Durée: {spectacle.duration}</p>
+          <p>Prix: {spectacle.price} €</p>
+        </li>
+      ))}
+    </ul>
+  );
 }
